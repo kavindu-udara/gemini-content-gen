6c9972db4cdfcde95552e0c5d7b65acc7ff45114
kavindu-udara <karunasinghesampath38@gmail.com>
2024-07-07 06:34:36 +0530

    create user test api

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 });
+
+app.use('/api/user', userRouter);
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
