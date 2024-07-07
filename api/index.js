0669ba2e5edd8317dd104270065b3e441ef13a4f
kavindu-udara <karunasinghesampath38@gmail.com>
2024-07-07 06:28:47 +0530

    create api

---
+++ api/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
