8ef00af85444042f47ca8e687d36c1a5f28e5e40
kavindu-udara <karunasinghesampath38@gmail.com>
2024-07-07 07:36:19 +0530

    create signup function

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,35 @@
+import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    console.log(hashedPassword);
+
+    if (!/\S+@\S+\.\S+/.test(email)) {
+        return res.status(400).json({
+            success: false,
+            message: "email must be a valid email address",
+        });
+    } else {
+        const hashedPassword = bcryptjs.hashSync(password, 10);
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword,
+        });
+        try {
+            await newUser.save();
+            // remove password
+            const {password, ...others} = newUser._doc;
+            res.status(201).json({
+                success: true,
+                user: others,
+                message: 'user created successfully',
+            });
+        } catch (err) {
+            res.status(500).json({
+                success: false,
+                message: err.message,
+            });
+        }
+    }
 }
